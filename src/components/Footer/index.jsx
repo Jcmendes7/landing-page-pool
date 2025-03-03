10e1f4ecdb4fa9eb7b01219b8db2bb5b5a471d55
Jcmendes7 <josecarlosms7f@gmail.com>
2025-03-02 23:40:08 -0300

    social media add

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,21 @@
 import './style.css'
 import '../../global.css'
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 export function Footer() {
     return (
       <footer className="footer">
-        <p>Entre em contato:</p>
-        <p>📞 <a href="tel:+5518996926998">(18) 996926998</a></p>
-        <p>📧 <a href="zpiscineiro@gmail.com">zpiscineiro@gmail.com</a></p>
-        <p>📍 Atendemos em Birigui-SP</p>
+        <div className='social-midia-container'>
+          <p id='social-midia'>/siga a gente</p>
+          <a href=""> <FaFacebookF size={25}/> </a>
+          <a href=""> <FaInstagram size={25}/> </a>
+        </div>
+        <div className='contato'>
+          <p >/Entre em contato:</p>
+          <p>📞 <a href="tel:+5518996926998">(18) 996926998</a></p>
+          <p>📧 <a href="zpiscineiro@gmail.com">zpiscineiro@gmail.com</a></p>
+          <p>📍 Atendemos em Birigui-SP</p>
+        </div>
       </footer>
     );
   }
